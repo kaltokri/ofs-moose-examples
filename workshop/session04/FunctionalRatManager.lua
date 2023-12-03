51f507d81d05bd0fb68d2f323543bf0eca622902
kaltokri <kaltokri@users.noreply.github.com>
2023-12-03 10:40:19 +0100

    Added av8b to RatManager mission and .gitignore file

--- workshop/session04/FunctionalRatManager.lua
+++ workshop/session04/FunctionalRatManager.lua
@@ -6,9 +6,16 @@ f15c:SetDeparture({"Kobuleti"})
 f15c:DestinationZone()
 f15c:SetDestination({"ZoneC"})
 
+local av8b=RAT:New("RAT_AV8B", "AV8B managed")
+av8b:SetDeparture({"ZoneC"})
+av8b:SetTakeoff("air")
+av8b:DestinationZone()
+av8b:SetDestination({"ZoneA"})
+
 local manager=RATMANAGER:New(25)
-manager:Add(a10c, 2)
-manager:Add(f15c, 2)
+manager:Add(a10c, 5)
+manager:Add(f15c, 5)
+manager:Add(av8b, 5)
 manager:Start(30)
 manager:Stop(7200)
 
